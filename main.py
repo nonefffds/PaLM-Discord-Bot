1068318e3447519f5cdd7f0bd973dfdeb79384db
FDS <fds@brunhild.cn>
2023-06-02 20:30:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import os
 import discord
 from src.discordBot import DiscordClient, Sender
 from src.logger import logger
-#from src.models import BardModel
 import google.generativeai as palm
 from src.server import keep_alive
 
-
+# Set up the PaLM API
 palm.configure(api_key = "BARD_API_KEY")
+model = palm.Model(output_token_limit=500)
 
 def chat_completion(message: str) -> str:
 # Get the PaLM API response
@@ -26,6 +26,9 @@ def run():
             return
         await interaction.response.defer()
         receive = chat_completion(message)
+        # Truncate the length of the message to 2000 characters, but set this to 2000 chars would still cause this problem. so I set it to 1900.
+        #if len(receive) > 1900:
+        #    receive = receive[:1900]
         await sender.send_message(interaction, message, receive)
 
 
