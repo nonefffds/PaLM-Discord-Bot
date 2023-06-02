f7f892d0f4dd444760783a33b169b56829b3b90c
FDS <fds@brunhild.cn>
2023-06-02 14:46:57 +0800

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,9 +1,9 @@
 from typing import List, Dict
 import google.generativeai as palm
 
-class ModelInterface:
-    def chat_completion(self, messages: List[Dict]) -> str:
-        pass
+#class ModelInterface:
+#    def chat_completion(self, messages: List[Dict]) -> str:
+#        pass
 
 class BardModel(ModelInterface):
     
@@ -11,6 +11,8 @@ class BardModel(ModelInterface):
                api_key: str):
         palm.configure(api_key = "BARD_API_KEY")
 
-    def chat_completion(self, messages) -> str:
-        response = palm.chat(messages=messages)
-        return response
+    def chat_completion(self, message) -> str:
+    # Get the PaLM API response
+        response = palm.chat(messages=message, candidate_count = 0)
+        output = response.messages[1]
+        print (output['content'])
